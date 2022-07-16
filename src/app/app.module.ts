b8fecbb2073d950a03df271e2eeb557410c51536
ylyanovikova <ylyanovikova@ukr.net>
2022-07-16 19:33:27 +0200

    lesson1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { CommemtsComponent } from './components/commemts/commemts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    CommemtsComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
