22d2a6d6d9240963eec87f3a79eeb8e9e50d282c
ylyanovikova <ylyanovikova@ukr.net>
2022-07-20 11:11:26 +0200

    comments

--- src/app/components/comment-details/comment-details.component.ts
+++ src/app/components/comment-details/comment-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ICommentDetails } from 'src/app/models';
+import { CommentsService } from 'src/app/services';
 
 @Component({
   selector: 'app-comment-details',
@@ -12,12 +13,17 @@ export class CommentDetailsComponent implements OnInit {
 
   commentDetails: ICommentDetails
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private commentService: CommentsService, private router: Router) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(value => {
-      let { state: { data } } = history;
-      this.commentDetails = data;
+    this.activatedRoute.params.subscribe(({ id }) => {
+      const state = this.router.getCurrentNavigation()?.extras?.state?.['data'] as ICommentDetails;
+
+      if (state) {
+        this.commentDetails = state;
+      } else {
+        this.commentService.getById(id).subscribe(value => this.commentDetails = value as ICommentDetails)
+      }
     });
   }
 }
